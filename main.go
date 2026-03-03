1b7cec3a02a341eba8df0233d42127a4fb0aaa64
cmaxwell <maxwellchibuezecharles@gmail.com>
2026-03-03 06:38:01 +0000

    incomplete calculator

--- main.go
+++ main.go
@@ -3,50 +3,40 @@ package main
 import(
 	"encoding/json"
 	"log"
+	"math"
 	"net/http"
+
+	"github.com/Knetic/govaluate"
 )
 
-type CalcRequest struct{
-	A float64 `json: "a"`
-	B float64 `json: "b"`
-	Op string `json: "op"`
+type Request struct{
+	Expr string `json:"expr"`
 }
-
-type CalcResponse struct{
-	Result float64 `json:"result"`
-	Error string `json:"error,omitemty"`
+type Response struct{
+	Result interface{} `json:"result"`
 }
 
 // the brain of the calculator
 func calculate(w http.ResponseWriter, r *http.Request) {
-	var req CalcRequest // were we store the data the user sent us
+	var req Request // were we store the data the user sent us
 
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		http.Error(w, "invalid input", http.StatusBadRequest)
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		json.NewEncoder(w).Encode(Response{Result: "mperi"})
+		return
 	}
 
-	var res CalcResponse // were we put the answers
-
-	switch req.Op {
-	case "+":
-		res.Result = req.A + req.B
-	case "-":
-		res.Result = req.A - req.B
-	case "*":
-		res.Result = req.A * req.B
-	case "/":
-		if req.B == 0 {
-			res.Error = "division by zero"
-		}else {
-			res.Result = req.A / req.B
-		}
-	default:
-		res.Error = "unknown operator"
+	
+	expression, err := govaluate.NewEvaluableExpression(req.Expr)
+	if err != nil {
+		json.NewEncoder(w).Encode(Response{Result: "mperi"})
+		return
 	}
 
-	w.Header().Set("Content-Type", "application/json") //here's my response in json format
-	json.NewEncoder(w).Encode(res)
-
+	result, err := expression.Evaluate(nil)
+	if err != nil {
+		json.NewEncoder(w).Encode(Response{Result: "mperi"})
+	}
 }
 
 func main() {
